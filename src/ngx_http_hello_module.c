299b97c7013746fdfb14f21a6949592ecfdf8045
u03013112@hotmail.com <u03013112@hotmail.com>
2020-04-26 15:39:04 +0800

    f

--- src/ngx_http_hello_module.c
+++ src/ngx_http_hello_module.c
@@ -17,6 +17,35 @@ static ngx_command_t ngx_http_hello_commands[] = {
     ngx_null_command
 };
 
+static ngx_http_module_t ngx_http_hello_module_ctx = {
+    NULL,   /* preconfiguration */
+    NULL,   /* postconfiguration */
+
+    NULL,   /* create main configuration */
+    NULL,   /* init main configuration */
+
+    NULL,   /* create server configuration */
+    NULL,   /* merge server configuration */
+
+    NULL,  /* create location configuration */
+    NULL,   /* merge location configuration */
+};
+
+ngx_module_t ngx_http_hello_module = {
+    NGX_MODULE_V1,
+    &ngx_http_hello_module_ctx,   /* module context */
+    ngx_http_hello_commands,      /* module directives */
+    NGX_HTTP_MODULE,                             /* module type */
+    NULL,                                        /* init master */
+    NULL,                                        /* init module */
+    NULL,   /* init process */
+    NULL,                                        /* init thread */
+    NULL,                                        /* exit thread */
+    NULL,   /* exit process */
+    NULL,                                        /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
 char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd,void *conf){
     ngx_http_core_loc_conf_t *clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
     clcf->handler = ngx_http_hello_handler;
