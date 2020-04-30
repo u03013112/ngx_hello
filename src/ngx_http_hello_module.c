7c05213956548a19a92a5439b9ef3a0cde681b59
u03013112@hotmail.com <u03013112@hotmail.com>
2020-04-30 17:02:38 +0800

    test

--- src/ngx_http_hello_module.c
+++ src/ngx_http_hello_module.c
@@ -89,17 +89,17 @@ static ngx_int_t ngx_http_hello_access_handler(ngx_http_request_t *r){
 }
 
 static ngx_int_t ngx_http_hello_init(ngx_conf_t *cf){
-    ngx_http_handler_pt        *h;
-    ngx_http_core_main_conf_t  *cmcf;
+    // ngx_http_handler_pt        *h;
+    // ngx_http_core_main_conf_t  *cmcf;
 
-    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_hello_module);
+    // cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_hello_module);
 
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
-    if (h == NULL) {
-        return NGX_ERROR;
-    }
+    // h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+    // if (h == NULL) {
+    //     return NGX_ERROR;
+    // }
 
-    *h = ngx_http_hello_access_handler;
+    // *h = ngx_http_hello_access_handler;
 
     return NGX_OK;
 }
